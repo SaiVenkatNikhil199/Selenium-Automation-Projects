517cc6bf9b9e33bc6af94d9b276f8cc581719228
neojarvis <techadmin@iamneo.ai>
2026-03-13 06:37:25 +0000

    second project from locators

--- Locators_q2_tatacliq_store/Project/src/test/java/runner/TestSpice.java
+++ Locators_q2_tatacliq_store/Project/src/test/java/runner/TestSpice.java
@@ -2,12 +2,14 @@ package runner;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.events.WebDriverListener;
@@ -18,18 +20,19 @@ import utils.EventHandler;
 import java.lang.Thread;
 public class TestSpice {
     public static WebDriver driver;
-    
+    public static Actions actions;
     @BeforeMethod
     public void openBrowser() throws MalformedURLException {
         try {
             ChromeOptions options = new ChromeOptions();
             driver = new RemoteWebDriver(new URL("http://localhost:4444/"), options);
+            actions = new Actions(driver);
             
             WebDriverListener listener = new EventHandler();
             driver = new EventFiringDecorator<>(listener).decorate(driver);
             driver.manage().window().maximize();
     
-            driver.get("https://www.thesouledstore.com/men");   
+            driver.get("https://www.tatacliq.com/");   
 
         } catch (Exception e) {
             // TODO: handle exception
@@ -40,17 +43,31 @@ public class TestSpice {
     @Test
     public void testMethod1(){
         try {
-            Thread.sleep(1000);
-            WebElement elem = driver.findElement(By.cssSelector("input[id='search']"));
-            elem.click();
-            elem.sendKeys("Shoes",Keys.ENTER);
+            WebElement categories = driver.findElement(By.xpath("//div[text()='Categories']"));
+            Actions actions = new Actions(driver);
+            actions.moveToElement(categories).perform();
+            Thread.sleep(4000);
+
+            WebElement mens_fashion = driver.findElement(By.xpath("//div[contains(@aria-label,\"Men's Fashion\")]"));
+            actions.moveToElement(mens_fashion).perform();
+            Thread.sleep(4000);
+            
+            WebElement t_shirts = driver.findElement((By.xpath("//a[text()='T-shirts']")));
+            t_shirts.click();
+            Thread.sleep(4000);
+            
+            WebElement first_t_shirt = driver.findElement(By.xpath("//div[@class='PlpComponent__base']/child::a[contains(@title,'Louis')]"));
+            first_t_shirt.click();
+            Thread.sleep(4000);
+
+            Set<String> windows = driver.getWindowHandles();
+            for (String window : windows) {
+                if(window != driver.getWindowHandle()) {
+                    driver.switchTo().window(window);
+                }
+            }
+
             Thread.sleep(3000);
-            WebElement productOne = driver.findElement(By.xpath("(//div[contains(@class,'productlist')])[1]"));
-            productOne.click();
-            Thread.sleep(4000);
-            WebElement wishList = driver.findElement(By.xpath("//span[text()='Add to Wishlist']"));
-            wishList.click();
-            Thread.sleep(4000);
         } catch (Exception e) {
             // TODO: handle exception
         }
