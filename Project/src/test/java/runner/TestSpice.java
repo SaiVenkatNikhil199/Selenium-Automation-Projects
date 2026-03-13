1f589a82d119203f78c50c6e5d519bd4df6b77b1
neojarvis <techadmin@iamneo.ai>
2026-03-13 14:04:11 +0000

    Manual-save-commit-13/03/2026--19:34:11

--- Project/src/test/java/runner/TestSpice.java
+++ Project/src/test/java/runner/TestSpice.java
@@ -32,28 +32,33 @@ public class TestSpice {
 
     @Test
     public void testMethod(){
-        WebElement contactUs = driver.findElement(By.xpath("(//a[text()='Contact Us'])[1]"));
-        contactUs.click();
-        
-        // After clicking the contact us , work on the form
-        WebElement name = driver.findElement(By.xpath("//input[@id='comment_name']"));
-        name.click();
-        name.sendKeys("John");
+        try{
 
-        WebElement email = driver.findElement(By.xpath("//input[@id='email']"));
-        email.click();
-        email.sendKeys("John@example.com");
-
-        WebElement subject = driver.findElement(By.xpath("//input[@id='subject']"));
-        subject.click();
-        subject.sendKeys("Refund");
-
-        WebElement message = driver.findElement(By.xpath("//textarea[@id='comment']"));
-        message.click();
-        message.sendKeys("This is a sample message");
-
-        WebElement sendButton = driver.findElement(By.xpath("//input[@id='submit']"));
-        sendButton.click();
+            WebElement contactUs = driver.findElement(By.xpath("(//a[text()='Contact Us'])[1]"));
+            contactUs.click();
+            Thread.sleep(2000);
+            // After clicking the contact us , work on the form
+            WebElement name = driver.findElement(By.xpath("//input[@id='comment_name']"));
+            name.click();
+            name.sendKeys("John");
+    
+            WebElement email = driver.findElement(By.xpath("//input[@id='email']"));
+            email.click();
+            email.sendKeys("John@example.com");
+    
+            WebElement subject = driver.findElement(By.xpath("//input[@id='subject']"));
+            subject.click();
+            subject.sendKeys("Refund");
+    
+            WebElement message = driver.findElement(By.xpath("//textarea[@id='comment']"));
+            message.click();
+            message.sendKeys("This is a sample message");
+    
+            WebElement sendButton = driver.findElement(By.xpath("//input[@id='submit']"));
+            sendButton.click();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 
