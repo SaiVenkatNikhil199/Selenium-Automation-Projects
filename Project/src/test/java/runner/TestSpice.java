b2af1fdbbf7516756722f2b4b2623fd0d8865d72
neojarvis <techadmin@iamneo.ai>
2026-03-14 10:46:48 +0000

    Auto-save-commit-14/03/2026--16:16:49

--- Project/src/test/java/runner/TestSpice.java
+++ Project/src/test/java/runner/TestSpice.java
@@ -36,7 +36,7 @@ public class TestSpice {
     @Test
     public void testMethod(){
         try{
-            Thread.sleep(2000);
+            
             WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
             username.click();
             username.sendKeys("standard_user");
@@ -47,7 +47,7 @@ public class TestSpice {
 
             WebElement login = driver.findElement(By.xpath("//input[@id='login-button']"));
             login.click();
-            Thread.sleep(2000);
+           
 
 
         }catch(Exception e){
