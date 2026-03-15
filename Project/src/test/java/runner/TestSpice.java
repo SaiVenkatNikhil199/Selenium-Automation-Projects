13e92ddfa8ff61767733c80a60d49a651b3e7019
neojarvis <techadmin@iamneo.ai>
2026-03-15 15:31:48 +0000

    Manual-save-commit-15/03/2026--21:01:49

--- Project/src/test/java/runner/TestSpice.java
+++ Project/src/test/java/runner/TestSpice.java
@@ -31,7 +31,7 @@ public class TestSpice {
         WebDriverListener listener = new EventHandler();
         driver = new EventFiringDecorator<>(listener).decorate(driver);
         driver.manage().window().maximize();
-        driver.get("https://testautomationpractice.blogspot.com/");
+        driver.get("https://qabrains.com/practice-site");
         
     }
 
