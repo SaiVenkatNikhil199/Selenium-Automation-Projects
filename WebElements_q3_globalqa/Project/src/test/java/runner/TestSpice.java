f490e21b0d9b47d3641b5577e2e9637d4e3d1b22
neojarvis <techadmin@iamneo.ai>
2026-03-16 11:55:55 +0000

    Auto-save-commit-16/03/2026--17:25:56

---
+++ WebElements_q3_globalqa/Project/src/test/java/runner/TestSpice.java
@@ -0,0 +1,71 @@
+package runner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import utils.EventHandler;
+
+public class TestSpice {
+    public static WebDriver driver;
+    
+    @BeforeMethod
+    public void openBrowser() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/"), options);
+        
+        WebDriverListener listener = new EventHandler();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+        driver.manage().window().maximize();
+        driver.get("https://www.globalsqa.com/");
+        
+    }
+
+    @Test
+    public void testMethod(){
+        try{
+            
+            WebElement contactUs = driver.findElement(By.xpath("(//a[text()='Contact Us'])[1]"));
+            contactUs.click();
+            Thread.sleep(2000);
+            // // After clicking the contact us , work on the form
+            WebElement name = driver.findElement(By.xpath("//input[@id='comment_name']"));
+            name.click();
+            name.sendKeys("John");
+    
+            WebElement email = driver.findElement(By.xpath("//input[@id='email']"));
+            email.click();
+            email.sendKeys("John@example.com");
+    
+            WebElement subject = driver.findElement(By.xpath("//input[@id='subject']"));
+            subject.click();
+            subject.sendKeys("Refund");
+    
+            WebElement message = driver.findElement(By.xpath("//textarea[@id='comment']"));
+            message.click();
+            message.sendKeys("This is a sample message");
+    
+            WebElement sendButton = driver.findElement(By.xpath("//input[@id='submit']"));
+            sendButton.click();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    @AfterMethod
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
